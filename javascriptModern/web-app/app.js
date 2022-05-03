d5f9c6f81e01e5674f99d6a678e5192f23612177
yashar6846 <yasharnajafi6846@gmail.com>
2022-05-03 11:37:27 +0300

    commit

---
+++ javascriptModern/web-app/app.js
@@ -0,0 +1,37 @@
+let products = getSeveProducts();
+
+const filters = {
+  searchItem: "",
+  availableProducts: false,
+};
+
+renderProducts(products, filters);
+
+document
+  .querySelector("#search-products")
+  .addEventListener("input", function (e) {
+    filters.searchItem = e.target.value;
+    renderProducts(products, filters);
+  });
+
+document
+  .querySelector("#add-product-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const id = uuidv4();
+    products.push({
+      id: id,
+      title: e.target.elements.productTitle.value,
+      exists: true,
+    });
+    seveProducts(products);
+    renderProducts(products, filters);
+    e.target.elements.productTitle.value = "";
+  });
+
+document
+  .querySelector("#available-products")
+  .addEventListener("change", function (e) {
+    filters.availableProducts = e.target.checked;
+    renderProducts(products, filters);
+  });
---
+++ javascriptModern/web-app/function.js
@@ -0,0 +1,54 @@
+const getSeveProducts = function () {
+  const productsJSON = localStorage.getItem("products");
+  if (productsJSON !== null) {
+    return JSON.parse(productsJSON);
+  } else {
+    return [];
+  }
+};
+const seveProducts = function (products) {
+  localStorage.setItem("products", JSON.stringify(products));
+};
+
+const renderProducts = function (products, filters) {
+  let filterdProducts = products.filter(function (item) {
+    return item.title.toLowerCase().includes(filters.searchItem.toLowerCase());
+  });
+  filterdProducts = filterdProducts.filter(function (item) {
+    if (filters.availableProducts) {
+      return item.exist;
+    } else {
+      return true;
+    }
+  });
+  document.querySelector("#products").innerHTML = "";
+  filterdProducts.forEach(function (item) {
+    document.querySelector("#products").appendChild(createProductDOM(item));
+  });
+};
+
+const createProductDOM = function (product) {
+  const productEl = document.createElement("div");
+  const checkbox = document.createElement("input");
+  const productItem = document.createElement("a");
+  const removeButten = document.createElement("button");
+
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.checked = !product.exist;
+  productEl.appendChild(checkbox);
+  checkbox.addEventListener("change", function () {
+    toggleProduct(product.id);
+    seveProducts(products);
+    renderProducts(products, filters);
+  });
+
+  productItem.textContent = product.title;
+  productItem.setAttribute("href", `./edit-product.html#${product.id}`);
+
+  productEl.appendChild(productItem);
+
+  removeButten.textContent = "Remove";
+  productEl.appendChild(removeButten);
+
+  return productEl;
+};
